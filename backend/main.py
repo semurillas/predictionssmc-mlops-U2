594436eabe62f4265e3a324460c4b3f91185cc46
semurillas <semurillas@gmail.com>
2025-05-18 21:04:10 -0500

    CI/CD en main: ejecutar pruebas y publicar imagen Docker

--- backend/main.py
+++ backend/main.py
@@ -4,6 +4,7 @@ from fastapi import FastAPI
 
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
+from predictor import evaluar_prediccion
 
 app = FastAPI()
 
@@ -14,8 +15,6 @@ app = FastAPI()
    # allow_methods=["*"],
     #allow_headers=["*"],
 #)
-
-
 
 app.add_middleware(
     CORSMiddleware,
@@ -42,32 +41,14 @@ class InputData(BaseModel):
 
 @app.post("/predict")
 def predict(data: InputData):
-    sintomas_graves = sum([
-        data.ictericia,
-        data.hinchazon_abdominal,
-        data.confusion,
-    ])
-
-    sintomas_moderados = sum([
-        data.dolor_abdominal,
-        data.fatiga_extrema,
-        data.orina_oscura,
-        data.heces_palidas
-    ])
-
-    total_sintomas = sintomas_graves + sintomas_moderados
-
-    if total_sintomas == 0 and data.edad < 60 and data.peso > 45:
-        resultado = "NO ENFERMO"
-    elif sintomas_moderados >= 1 and sintomas_graves == 0:
-        resultado = "ENFERMEDAD LEVE"
-    elif sintomas_moderados >= 2 and sintomas_graves == 1:
-        resultado = "ENFERMEDAD AGUDA"
-    elif sintomas_graves >= 2 or (data.edad >= 65 and sintomas_moderados >= 2):
-        resultado = "ENFERMEDAD CRÓNICA"
-    elif sintomas_graves == 3 or (data.confusion and data.edad >= 70 and data.peso < 50):
-        resultado = "ENFERMEDAD TERMINAL"
-    else:
-        resultado = "NO ENFERMO"
-
+    sintomas = {
+        "ictericia": data.ictericia,
+        "dolor_abdominal": data.dolor_abdominal,
+        "fatiga_extrema": data.fatiga_extrema,
+        "hinchazon_abdominal": data.hinchazon_abdominal,
+        "confusion": data.confusion,
+        "orina_oscura": data.orina_oscura,
+        "heces_palidas": data.heces_palidas
+    }
+    resultado = evaluar_prediccion(data.edad, data.peso, sintomas)
     return {"resultado": resultado}
